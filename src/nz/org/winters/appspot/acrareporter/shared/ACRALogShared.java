330163e0ee89141205e153037996d9f81a89b5df
mathew <mathew@winters.org.nz>
2013-01-07 22:19:30 +0000

    add license headers.
    /*
     * Copyright 2013 Mathew Winters
    
     * Licensed under the Apache License, Version 2.0 (the "License");
     * you may not use this file except in compliance with the License.
     * You may obtain a copy of the License at
    
           http://www.apache.org/licenses/LICENSE-2.0
    
     * Unless required by applicable law or agreed to in writing, software
     * distributed under the License is distributed on an "AS IS" BASIS,
     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     * See the License for the specific language governing permissions and
     * limitations under the License.
    */

--- src/nz/org/winters/appspot/acrareporter/shared/ACRALogShared.java
+++ src/nz/org/winters/appspot/acrareporter/shared/ACRALogShared.java
@@ -3,15 +3,24 @@ package nz.org.winters.appspot.acrareporter.shared;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.servlet.http.HttpServletRequest;
-
 import com.google.gwt.user.client.rpc.IsSerializable;
-import com.googlecode.objectify.annotation.Entity;
-import com.googlecode.objectify.annotation.Id;
-import com.googlecode.objectify.annotation.Index;
-import com.googlecode.objectify.annotation.Serialize;
-import com.googlecode.objectify.annotation.Unindex;
+/*
+ * Copyright 2013 Mathew Winters
 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+// Serializable version of ACRALog
 public class ACRALogShared implements Serializable, IsSerializable
 {
   /**
