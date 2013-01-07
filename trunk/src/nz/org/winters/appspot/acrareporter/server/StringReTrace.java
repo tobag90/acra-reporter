19694e5df91dd425e9f8550e05b2137af338a2b7
mathew@winters.org.nz <mathew@winters.org.nz@469bd3e1-1316-ac64-b521-4ac7ffa2805b>
2013-01-07 22:19:30 +0000

    add license headers.
    /*
     * Copyright 2013 Mathew Winters
    
     * Licensed under the Apache License, Version 2.0 (the "License");
     * you may not use this file except in compliance with the License.
     * You may obtain a copy of the License at
    
           http://www.apache.org/licenses/LICENSE-2.0
    
     * Unless required by applicable law or agreed to in writing, software
     * distributed under the License is distributed on an "AS IS" BASIS,
     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     * See the License for the specific language governing permissions and
     * limitations under the License.
    */

--- trunk/src/nz/org/winters/appspot/acrareporter/server/StringReTrace.java
+++ trunk/src/nz/org/winters/appspot/acrareporter/server/StringReTrace.java
@@ -19,14 +19,21 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package nz.org.winters.appspot.acrareporter.server;
+/*
+ * Copyright 2013 Mathew Winters
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.io.Reader;
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -39,7 +46,6 @@ import java.util.regex.Pattern;
 
 import proguard.classfile.util.ClassUtil;
 import proguard.obfuscate.MappingProcessor;
-import proguard.obfuscate.MappingReader;
 
 /**
  * Tool for de-obfuscating stack traces of applications that were obfuscated
